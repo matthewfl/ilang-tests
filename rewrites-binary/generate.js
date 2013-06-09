4d416bfa2d61584b1dcd41d895919e05dc0f7723
Matthew Francis-Landau <matthew@matthewfl.com>
2013-06-09 09:26:22 -0700

    added result of generating random bits

--- rewrites-binary/generate.js
+++ rewrites-binary/generate.js
@@ -1,5 +1,21 @@
-var fs = require('fs');
-
 var bits = process.argv[process.argv.length-1] * 1;
 
 console.error("Generating for", bits);
+
+var items = 0x1 << bits;//Math.pow(2, bits);
+
+var result = [];
+
+for(var a=0; a < items; a++) {
+    var line = "";
+    var mask = 0x1 << (bits -1);
+    line += (Math.random() > .5 ? "1" : "0");
+    while(mask) {
+	line += "," + (mask & a ? "1" : "0");
+	mask = mask >> 1;
+    }
+    console.log(line);
+    //result.push(line);
+}
+
+//console.log(result.join('\n'));
