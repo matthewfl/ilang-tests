cf179d10308b6f135c8be1b1089b41fc91e5b0e0
Matthew Francis-Landau <matthew@matthewfl.com>
2013-06-23 21:07:30 -0700

    change generate to use json instead of csv

--- rewrites-binary/generate.js
+++ rewrites-binary/generate.js
@@ -7,15 +7,16 @@ var items = 0x1 << bits;//Math.pow(2, bits);
 var result = [];
 
 for(var a=0; a < items; a++) {
-    var line = "";
+    var line = []
     var mask = 0x1 << (bits -1);
-    line += (Math.random() > .5 ? "1" : "0");
+    line.push(Math.random() > .5)
     while(mask) {
-	line += "," + (mask & a ? "1" : "0");
+	line.push(mask & a ? true : false);
 	mask = mask >> 1;
     }
-    console.log(line);
-    //result.push(line);
+    //console.log(line);
+    result.push(line);
 }
 
+console.log(JSON.stringify(result, null, 1))
 //console.log(result.join('\n'));
